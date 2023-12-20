43e71e509000fd6ce469d90c614706ed648fbd8e
MrQuba <148873792+MrQuba@users.noreply.github.com>
2023-12-20 21:20:33 +0100

    Source code update
    passwordGen.cpp update
    added precompiled binary

--- passwordGen.cpp
+++ passwordGen.cpp
@@ -3,11 +3,12 @@
 #include <cstring>
 using namespace std;
 int main() {
+	int passLength;
+	cout << "Input length of the password: " << endl;
+	cin >> passLength;
     const char* acceptableCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:',.<>/?`~";
     srand(time(NULL));
     int length=strlen(acceptableCharacters);
-    int passLength;
-    cin >> passLength;
     char result[passLength];
     int current;
     for(int i=0; i < passLength; i++){
